85993d0449cfab87668a929f65f213162f2b5542
Daniel Nguyen <danngu@uw.edu>
2020-05-17 01:28:39 -0700

    add my repository

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,7 +1,7 @@
 // -- Prismic API endpoint
 // Determines which repository to query and fetch data from
 // Configure your site's access point here
-export const apiEndpoint = 'https://football-blog-example.prismic.io/api/v2'
+export const apiEndpoint = 'https://folio72.prismic.io/api/v2'
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
